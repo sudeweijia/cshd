0ca94008304b0193b6a890ca72f90ea6f57cd6c1
sudeweijia <1494130690@qq.com>
2025-03-23 14:52:14 +0800

    Update Message.js

--- server/Message.js
+++ server/Message.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
+// 定义留言模型
 const MessageSchema = new mongoose.Schema({
     message: {
         type: String,
-        required: true,
+        required: true, // 留言内容必填
     },
     createdAt: {
         type: Date,
-        default: Date.now,
+        default: Date.now, // 默认值为当前时间
     },
 });
 
+// 导出模型
 module.exports = mongoose.model('Message', MessageSchema);
